b6ace263d0a4607f60f23d38b3ea7765b4c1358c
Alvin Zulham <alvinzulhamm@gmail.com>
2022-10-26 09:38:34 +0700

    page status

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -8,14 +8,17 @@ class PageController extends Controller
 {
     public function Index(Request $request)
     {
+        // main page
         return view('index');
     }
     public function About(Request $request)
     {
+        // about page
         return view('about');
     }
     public function News(Request $request)
     {
+        // news about product
         return view('news');
     }
     public function Draft(Request $request)
